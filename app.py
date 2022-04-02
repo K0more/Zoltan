f7c3154e7a793e4bb730044d015885afefd3992b
finite-simple-group-of-order-two <ashutosh200210@outlook.com>
2022-04-03 00:00:59 +0530

    implement demo auth system

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from typing import final
 import handlers
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI, HTTPException, Request, WebSocket, WebSocketDisconnect
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -12,9 +12,37 @@ connection_handler = handlers.ConnectionHandler()
 
 cur_id = 0
 
+fake_db = {
+    "Mike": {
+        "password": "fakepassword123"
+    },
+    "John": {
+        "password": "fakepassword123"
+    }
+}
+
 @app.route('/')
 async def home(request):
     return templates.TemplateResponse('chat.html', {'request': request})
+
+@app.post('/auth')
+async def auth(request: Request):
+    data = await request.json()
+    username = data.get('username')
+    password = data.get('password')
+    data = await request.json()
+    if not username or not password:
+        raise HTTPException(400, "Username or password not specified")
+    
+    user_obj = fake_db.get(username)
+
+    if not user_obj:
+        raise HTTPException(403, "Incorrect username")
+    
+    if not password == user_obj['password']:
+        raise HTTPException(403, "Incorrect password")
+
+    return {'data': {'username': username}}
 
 @app.websocket('/chat')
 async def ws_connect(ws: WebSocket):
