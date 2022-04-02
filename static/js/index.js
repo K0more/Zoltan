949af5d73e0d5183ae6238b853e9fc31616a4efe
finite-simple-group-of-order-two <ashutosh200210@outlook.com>
2022-04-02 23:33:06 +0530

    implement copy source button

--- static/js/index.js
+++ static/js/index.js
@@ -11,9 +11,10 @@ function createMessageNode(text, id, source){
         <img src="/static/img/guy.jpeg" alt="placeholder" class="pfp-img">
         </div>
         <div class="content">
-            <div class="username">Mike <span onclick=showSource(${id})> <img src="static/img/code.webp" height="15px" style="cursor: pointer"> </span> </div>
+            <div class="username">Mike <span onclick=showSource(${id})> <img src="static/img/code.webp" height="15px" style="cursor: pointer" class="source-btn"> </span> </div>
             <div>${text}</div>
             <div id="sid${id}" class="source">
+                <div id="copyBtn" onclick="copySourceToClipboard(${id})" onmouseout="resetSourceCopyBtn(${id})"> <img src="static/img/outline_content_copy_black_18dp.png" height="15px" class="source-copy-btn" id="scb${id}"></div>
                 ${source}
             </div>
         </div>
--- static/js/ui.js
+++ static/js/ui.js
@@ -6,13 +6,14 @@ function keyPressHandler(e) {
         previewBox.innerHTML = ""
     }
 }
+messageBox.addEventListener("keypress", keyPressHandler)
 
 messageBox.addEventListener("keyup", function (e) {
     previewBox.style.display = "block";
     messageText = messageBox.value
     sanitized = sanitizeHTML(messageText)
     previewBox.innerHTML = `Preview: <br> ${renderTex(sanitized)}`
-    if (messageBox.value.trim() == ""){
+    if (messageBox.value.trim() == "") {
         messageBox.value = ""
         messageBox.style.height = "2rem"
         previewBox.style.display = "none";
@@ -26,11 +27,7 @@ messageBox.addEventListener("keydown", function (e) {
     if (e.key != "Tab") {
         return;
     }
-
     e.preventDefault()
-    // console.log(this.selectionStart)
-    // words = this.value.split(" ")
-    // // curword = words[words.length-1]
     let curword = ""
     for (let i = this.selectionStart - 1; i >= 0; i--) {
         if (this.value[i] === "\\") {
@@ -39,53 +36,55 @@ messageBox.addEventListener("keydown", function (e) {
                 return;
             }
             let cur_idx = this.selectionEnd;
-            // console.log(this.value);
-            // console.log(`i is ${i}, sstart is ${this.selectionStart}`)
             curword = this.value.substring(i + 1, this.selectionStart);
 
-            // console.log(curword)
             let restOfTheString = this.value.substring(0, i + 1) + this.value.substring(this.selectionStart, this.value.length)
             for (let el of texts) {
                 if (el.startsWith(curword)) {
                     this.value = this.value.substring(0, i + 1) + el + this.value.substring(this.selectionStart, this.value.length)
                     let factor = 0
-                    if (el.endsWith("{}{}")){
+                    if (el.endsWith("{}{}")) {
                         factor = 3
-                    } else if(el.endsWith("{}")){
+                    } else if (el.endsWith("{}")) {
                         factor = 1
                     }
                     this.selectionStart = cur_idx + el.length - curword.length - factor
                     this.selectionEnd = cur_idx + el.length - curword.length - factor
                 }
             }
-            // console.log(restOfTheString)
-
             break;
         }
     }
-
-    /*for (let el of texts){
-          if (el.startsWith(curword)){
-         this.value += 	el.substr(curword.length, el.length);
-      }
-    }*/
 });
 
-function showSource(inp){
+function showSource(inp) {
     let sourcediv = document.getElementById(`sid${inp}`)
-    if (sourcediv.style.display == "none"){
-        sourcediv.style.display = "block"
-    } else {
+    if (sourcediv.style.display == "block") {
         sourcediv.style.display = "none"
+    } else {
+        sourcediv.style.display = "block"
     }
 }
 
-function textAreaAdjust(element){
-    if(element.value.split("\n").length < 2){
+function textAreaAdjust(element) {
+    if (element.value.split("\n").length < 2) {
         return
     }
     element.style.height = "1px"
-    element.style.height = (25+element.scrollHeight)+"px";
+    element.style.height = (25 + element.scrollHeight) + "px";
 }
 
-messageBox.addEventListener("keypress", keyPressHandler)
+function copySourceToClipboard(id){
+    let el = document.getElementById(`sid${id}`)
+    let btn = document.getElementById(`scb${id}`)
+    navigator.clipboard.writeText(el.innerText)
+    btn.src = "static/img/task-completed.png"
+    btn.style.opacity = "100%"
+    btn.style.height = "20px"
+}
+
+function resetSourceCopyBtn(id){
+    let btn = document.getElementById(`scb${id}`)
+    btn.src = "static/img/outline_content_copy_black_18dp.png"
+    btn.style.height = "15px"
+}
