520d3accb40f505783584bd7a12e6925578f071e
finite-simple-group-of-order-two <ashutosh200210@outlook.com>
2022-04-02 21:16:55 +0530

    client side changes for message ids

--- static/js/websocket.js
+++ static/js/websocket.js
@@ -1,10 +1,12 @@
 var ws = new WebSocket("ws://localhost:8000/chat")
 
 ws.onmessage = function(event){
-    let sanitized = sanitizeHTML(event.data)
+    let data = JSON.parse(event.data)
+    let sanitized = sanitizeHTML(data.data)
     let rendered = renderTex(sanitized)
-    messages.appendChild(createMessageNode(rendered))
+    messages.appendChild(createMessageNode(rendered, data.id, sanitized))
     messages.scrollTop = messages.scrollHeight
+    messageBuffer[data.id] = sanitized
 }
 
 function sendMessage(message){
